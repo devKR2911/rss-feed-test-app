76335c76110fc431bc1b32d0f7913819e1622ae8
pranav <pranavcbalan@gmail.vcom>
2020-11-15 16:03:24 +0530

    feat: added loader

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,6 +2,6 @@ import {Navbar} from 'react-bootstrap';
 
 export default function Header() {
   return <Navbar bg="light" expand="lg">
-    <Navbar.Brand>RSS FEED APP</Navbar.Brand>
+    <Navbar.Brand href="/">RSS FEED APP</Navbar.Brand>
   </Navbar>;
 }
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,17 +1,18 @@
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import { Alert, Card } from 'react-bootstrap';
+import {useRouter} from 'next/router';
+import {useEffect, useState} from 'react';
+import {Alert, Card} from 'react-bootstrap';
 
 export default function Feed() {
   const router = useRouter();
   const [settings, setSettings] = useState(null);
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const loadItemsAndSettings = async () => {
     // const data = await fetch(`/api/feeds/${router.query.id}`);
     const data = await fetch('/graphql', {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         query: `{
            FeedDataRead(id: ${JSON.stringify(router.query.id)}){
@@ -35,7 +36,8 @@ export default function Feed() {
         }`,
       }),
     });
-    const { feed, settings } = (await data.json())?.data?.FeedDataRead;
+    setLoading(false);
+    const {feed, settings} = (await data.json())?.data?.FeedDataRead;
     setSettings(settings);
     setData(feed);
   };
@@ -46,22 +48,27 @@ export default function Feed() {
 
   return (
     <div className="container mt-1 mb-1">
-      {data && (
-        <>
-          <h3 className="text-capitalize mt-3 mb-3 text-center">{data.title}</h3>
 
-          {data.items?.length ? (
-            data.items.map((item, i) => (
-              <Card
-                className="mt-2 mx-auto"
-                key={i}
-                style={{
-                  width: settings?.width ? `${settings.width}px` : 'auto',
-                  height: settings?.height ? `${settings.height}px` : 'auto',
-                  overflow: 'auto',
-                }}
-              >
-                <Card.Header>
+      {loading ?
+        <div className="justify-content-center d-flex align-content-center mt-4">
+          <span>Loading please wait &nbsp;&nbsp;&nbsp;</span><i className="fas fa-circle-notch fa-spin"
+                                                                style={{fontSize: '30px'}}></i></div>
+        : (data ? (
+              <>
+                <h3 className="text-capitalize mt-3 mb-3 text-center">{data.title}</h3>
+
+                {data.items?.length ? (
+                  data.items.map((item, i) => (
+                    <Card
+                      className="mt-2 mx-auto"
+                      key={i}
+                      style={{
+                        width: settings?.width ? `${settings.width}px` : 'auto',
+                        height: settings?.height ? `${settings.height}px` : 'auto',
+                        overflow: 'auto',
+                      }}
+                    >
+                      <Card.Header>
                   <span
                     style={{
                       fontSize: settings?.headerFontSize || '18px',
@@ -70,23 +77,24 @@ export default function Feed() {
                   >
                     {item.title}
                   </span>
-                </Card.Header>
-                <Card.Body
-                  style={{
-                    fontSize: settings?.contentFontSize || '12px',
-                    color: settings?.contentColor || '#000',
-                    backgroundColor: settings?.backgroundColor || '#fff',
-                  }}
-                >
-                  <div dangerouslySetInnerHTML={{ __html: item.content }}></div>
-                </Card.Body>
-              </Card>
-            ))
-          ) : (
-            <Alert variant="info">No feed items found</Alert>
-          )}
-        </>
-      )}
+                      </Card.Header>
+                      <Card.Body
+                        style={{
+                          fontSize: settings?.contentFontSize || '12px',
+                          color: settings?.contentColor || '#000',
+                          backgroundColor: settings?.backgroundColor || '#fff',
+                        }}
+                      >
+                        <div dangerouslySetInnerHTML={{__html: item.content}}></div>
+                      </Card.Body>
+                    </Card>
+                  ))
+                ) : (
+                  <Alert variant="info">No feed items found</Alert>
+                )}
+              </>)
+            : <Alert variant="info">Not found</Alert>
+        )}
     </div>
   );
 }
