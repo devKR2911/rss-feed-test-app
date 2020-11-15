b106e6cb04d060abe3d7542ebc877a7c2c77943d
pranav <pranavcbalan@gmail.vcom>
2020-11-15 22:45:19 +0530

    fix: fetching image on update

--- components/FeedsList.tsx
+++ components/FeedsList.tsx
@@ -1,72 +1,60 @@
 import './FeedsList.module.css';
-import {Alert, Card, Col, ListGroup, ListGroupItem, Row} from 'react-bootstrap';
+import { Alert, Card, Col, Row } from 'react-bootstrap';
 
-export default function FeedList({feeds, onEdit, onDelete, onView}) {
+export default function FeedList({ feeds, onEdit, onDelete, onView }) {
+  const getDomain = (host) => {
+    return host.match(/https?:\/\/([^\/]+)/)?.[1];
+  };
+
   return (
     <div className="container container-fluid">
       {feeds?.length ? (
-        /*     <div className=" mt-2 pt-2">
-               <ListGroup>
-                 {feeds.map((feed, i) => (
-                   <ListGroup.Item key={i} className="d-flex  align-items-center list-item">
-                     <span>{feed.feedUrl}</span>
-                     <span className="flex-grow-1"/>
-                     <div className="action-icons">
-                     <span className=" ml-4 mr-3 flex-shrink-1 edit-btn"
-                           title="Edit settings of feed" onClick={() => onEdit(feed)}>
-                       <i className="fa fa-cog  icn-btn" />
-                     </span>
-                     <span className="mr-3 flex-shrink-1 view-btn"
-                           title="View RSS feed" onClick={() => onView(feed._id)}>
-                       <i className="fa fa-eye  icn-btn"/>
-                     </span>
-                     <span className="flex-shrink-1 delete-btn" title="Delete RSS feed" onClick={() => onDelete(feed._id)}>
-                       <i className="fa fa-times  icn-btn"/>
-                     </span>
-                     </div>
-                   </ListGroup.Item>
-                 ))}
-               </ListGroup>
-             </div>*/
-        <Row className=" mt-2 pt-2">
+        <Row className=" mt-2 pt-2 d-flex align-items-stretch">
           {feeds.map((feed, i) => (
-            <Col md={6} lg={4} xl={3} key={i} className="pb-4" style={{position: 'relative'}}>
-              <Card style={{width: '100%'}}>
+            <Col md={6} lg={4} xl={3} key={i} className="pb-4" style={{ position: 'relative' }}>
+              <Card className="feed-card">
                 <Card.Body>
-                  <Card.Img  src={feed.image}/>
-                  <Card.Title className="feed-card-title"><a href={feed.feedUrl}
-                                                             target="_blank">{feed.feedUrl}</a></Card.Title>
-                  <Card.Text>
-                    {feed.description}
-                  </Card.Text>
+                  <Card.Body className="d-flex align-items-center" style={{ height: '250px' }}>
+                    <Card.Img src={feed.image} />
+                  </Card.Body>
+                  <Card.Title className="feed-card-title">
+                    <a href={feed.feedUrl} target="_blank">
+                      {getDomain(feed.feedUrl)}
+                    </a>
+                  </Card.Title>
+                  <Card.Text>{feed.description}</Card.Text>
                 </Card.Body>
-                {/*<ListGroup className="list-group-flush">*/}
-                {/*  <ListGroupItem>Cras justo odio</ListGroupItem>*/}
-                {/*</ListGroup>*/}
-                <Card.Body>
+                <Card.Body className="d-flex align-items-end p-0">
                   <div className="action-icons">
-                              <span className="flex-shrink-1 view-btn"
-                                    title="View RSS feed" onClick={() => onView(feed._id)}>
-                  <i className="fa fa-eye  icn-btn"/>
-                </span>
-                    <span className=" flex-shrink-1 edit-btn"
-                          title="Edit settings of feed" onClick={() => onEdit(feed)}>
-                  <i className="fa fa-cog  icn-btn"/>
-                </span>
+                    <span className="flex-shrink-1 view-btn" title="View RSS feed" onClick={() => onView(feed._id)}>
+                      <i className="fa fa-eye  icn-btn" />
+                    </span>
+                    <span
+                      className=" flex-shrink-1 edit-btn"
+                      title="Edit settings of feed"
+                      onClick={() => onEdit(feed)}
+                    >
+                      <i className="fa fa-cog  icn-btn" />
+                    </span>
 
-                    <span className="flex-shrink-1 delete-btn" title="Delete RSS feed"
-                          onClick={() => onDelete(feed._id)}>
-                  <i className="fa fa-times  icn-btn"/>
-                </span>
+                    <span
+                      className="flex-shrink-1 delete-btn"
+                      title="Delete RSS feed"
+                      onClick={() => onDelete(feed._id)}
+                    >
+                      <i className="fa fa-times  icn-btn" />
+                    </span>
                   </div>
                 </Card.Body>
               </Card>
             </Col>
-            ))}
-            </Row>
-            ) : (
-            <Alert variant="info" className="mt-4">No saved RSS feed found</Alert>
-            )}
-        </div>
-      );
-      }
+          ))}
+        </Row>
+      ) : (
+        <Alert variant="info" className="mt-4">
+          No saved RSS feed found
+        </Alert>
+      )}
+    </div>
+  );
+}
